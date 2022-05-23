a1fda7e654c4e3cc2592e954f976304f53542a0c
Vinuraj Devaraj <vinurajd@gmail.com>
2022-05-23 16:24:56 -0500

    updated

--- Analysis_Model.R
+++ Analysis_Model.R
@@ -19,11 +19,12 @@ library(car)
 file_path = "D:/PhD Program/Final Research/Dissertation/carnatic_raga_MER_dataset.csv"
 
 mer_data_raw = read.csv(file_path, header = T, stringsAsFactors = F)
-mer_data = mer_data_raw[mer_data_raw$Quadrant=='Q1',]
+#mer_data = mer_data_raw[mer_data_raw$Quadrant=='Q1',]
+mer_data = mer_data_raw
 
 # Step 1: Summary & Basic plots
 summary(mer_data)
-mer_data$perc <- mer_data$perc + 1.0 # to avoid 0 values
+mer_data$perc <- mer_data$perc + 1.0 # to avoid percentages with '0.00' as value
 mer_data$Emotion <- factor(mer_data$Emotion)
 mer_data$Quadrant <- factor(mer_data$Quadrant)
 mer_data$raga <- factor(mer_data$raga)
@@ -72,7 +73,7 @@ kruskal.test(y_log~raga, data = mer_data)
 
 # Step 2: One-way ANOVA
 # raga - linear model
-aov_model_raga <- aov(perc~raga-1, data = mer_data)
+aov_model_raga <- aov(perc~raga, data = mer_data)
 summary(aov_model_raga)
 qqnorm(aov_model_raga$residuals)
 
@@ -82,6 +83,7 @@ marginal = lsmeans(aov_model_raga,
 
 pairs_df = pairs(marginal, adjust = 'tukey')
 #write.csv(pairs_df,"test_csv.csv")
+pairs_df
 
 
 CLD = cld(marginal,
@@ -92,3 +94,113 @@ CLD
 
 TukeyHSD(aov_model_raga)
 Anova(aov_model_raga, type='2')
+
+
+# Quadrant - linear model
+aov_model_quadrant <- aov(perc~raga, data = mer_data)
+summary(aov_model_quadrant)
+qqnorm(aov_model_quadrant$residuals)
+
+Anova(aov_model_quadrant, type='II')
+marginal = lsmeans(aov_model_quadrant,
+                   ~ raga)
+
+pairs_df = pairs(marginal, adjust = 'tukey')
+#write.csv(pairs_df,"test_csv.csv")
+pairs_df
+
+
+CLD = cld(marginal,
+          alpha   = 0.05,
+          Letters = letters,         ###  Use lowercase letters for .group
+          adjust  = "tukey") 
+CLD
+
+TukeyHSD(aov_model_quadrant)
+Anova(aov_model_quadrant, type='2')
+
+# 2-Way ANOVA
+
+aov_model <- aov(perc~raga+Quadrant, data = mer_data)
+summary(aov_model)
+qqnorm(aov_model$residuals)
+
+Anova(aov_model, type='II')
+marginal = lsmeans(aov_model,
+                   ~ raga)
+
+pairs_df = pairs(marginal, adjust = 'tukey')
+#write.csv(pairs_df,"test_csv.csv")
+pairs_df
+
+
+CLD = cld(marginal,
+          alpha   = 0.05,
+          Letters = letters,         ###  Use lowercase letters for .group
+          adjust  = "tukey") 
+CLD
+
+TukeyHSD(aov_model)
+Anova(aov_model, type='2')
+
+# 2-Way ANOVA no intercept
+
+# Fixed effects
+aov_model_ni <- aov(perc~raga+Quadrant, data = mer_data)
+
+# Interaction effect
+aov_model_ni <- aov(perc~raga+Quadrant+raga:Quadrant, data = mer_data)
+
+#OR
+aov_model_ni <- aov(perc~raga*Quadrant-1, data = mer_data)
+summary(aov_model_ni)
+qqnorm(aov_model_ni$residuals)
+
+Anova(aov_model_ni, type='II')
+marginal = lsmeans(aov_model_ni,
+                   ~ raga)
+
+pairs_df = pairs(marginal, adjust = 'tukey')
+#write.csv(pairs_df,"test_csv.csv")
+pairs_df
+
+
+CLD = cld(marginal,
+          alpha   = 0.05,
+          Letters = letters,         ###  Use lowercase letters for .group
+          adjust  = "tukey") 
+CLD
+
+tt <- TukeyHSD(aov_model_ni)
+Anova(aov_model_ni, type='2')
+
+pairwise.t.test(mer_data$perc,mer_data$Emotion,p.adj="bonferroni")
+pairwise.t.test(mer_data$perc,mer_data$raga,p.adj="bonferroni")
+
+
+TukeyHSD(x = aov_model_ni,
+         ordered = FALSE,
+         which = "Quadrant",
+         conf.level = 0.95)
+
+summary(aov_model_ni)
+
+aov_model_ni <- aov(perc~Quadrant+(1/raga), data = mer_data)
+
+
+aov_model_ni <- aov(perc~raga+Quadrant-1, data = mer_data)
+
+summary(aov_model_ni)
+qqnorm(aov_model_ni$residuals)
+
+Anova(aov_model_ni, type='II')
+
+TukeyHSD(x = aov_model_ni,
+         ordered = FALSE,
+         which = "Quadrant",
+         conf.level = 0.95)
+
+interaction.plot(mer_data$raga,mer_data$Quadrant,mer_data$perc)
+mer_data_q1 = mer_data_raw[mer_data_raw$raga %in% c('Mayamalavagaula','Bhairavi'),]
+
+pairwise.t.test(mer_data_q1$perc, mer_data_q1$raga, adjust.method="BH")
